437ec9aa16a37816148d0d65141e50025c8c36cf
Fitri Maharani <fitrim1411@gmail.com>
2026-05-17 06:12:23 +0700

    Initial Commit

---
+++ StrukScan/StrukScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrukScanApp.swift
+//  StrukScan
+//
+//  Created by Fitri Maharani on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct StrukScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
